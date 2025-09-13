2a4611e50da07b467236104435c24907a4159b2d
MateusK-Martins <teteukm12345@gmail.com>
2025-09-12 22:44:08 -0300

    just matrices

--- src/lib.zig
+++ src/lib.zig
@@ -1,10 +1,19 @@
-const std = @import("std");
+pub const std = @import("std");
 pub const matrix = @import("matrix/matrix.zig");
-pub const layer = @import("layers/dense/dense.zig");
-pub const context = @import("optimization/context.zig");
-pub const activation = @import("optimization/activation.zig");
-pub const layer_kinds = @import("layers/layer_kinds.zig");
 
 pub fn main() void {
+    var a = matrix.Matrix(2, 2).init();
+    a.data = [_]f32{ 1.0, 2.0, 3.0, 4.0 };
 
+    var b = matrix.Matrix(2, 2).init();
+    b.data = [_]f32{ 5.0, 6.0, 7.0, 8.0 };
+
+    const result = a
+        .Add(.InplaceChain, &b)         
+        .Sub(.InplaceChain, &b)   
+        .Add(.InplaceChain, &a); 
+
+    for (result.data, 0..) |val, i| {
+        std.debug.print("[{}] = {}\n", .{ i, val });
+    }
 }
--- src/test_all.zig
+++ src/test_all.zig
@@ -2,5 +2,4 @@ const std = @import("std");
 
 test {
     _ = @import("matrix/test.zig");
-    _ = @import("layers/dense/test.zig");
 }
